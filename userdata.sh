f1a1ee42c396872cd2ee637977dd50cea0f94fdc
mandavikas17 <vikasmanda1249@gmail.com>
2025-05-27 18:33:27 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-# Update all packages
+# Update packages
 yum update -y
 
 # Install dependencies
@@ -10,23 +10,44 @@ yum install -y git curl wget conntrack
 # Enable and install Docker
 amazon-linux-extras enable docker
 yum install -y docker
-systemctl start docker
 systemctl enable docker
+systemctl start docker
 usermod -aG docker ec2-user
 
-# Install kubectl (latest stable version)
+# Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/local/bin/
+chown ec2-user:ec2-user /usr/local/bin/kubectl
 
-# Install Minikube (latest version)
+# Install Minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
 mv minikube /usr/local/bin/
+chown ec2-user:ec2-user /usr/local/bin/minikube
 
-# Fix PATH and permissions for ec2-user
+# Fix PATH
 echo 'export PATH=$PATH:/usr/local/bin' >> /home/ec2-user/.bash_profile
 chown ec2-user:ec2-user /home/ec2-user/.bash_profile
 
-# Start Minikube on first login (not in cloud-init, safer)
-echo 'if ! minikube status &>/dev/null; then minikube start --driver=none; fi' >> /home/ec2-user/.bashrc
+# Create systemd service to auto-start Minikube as ec2-user
+cat <<EOF > /etc/systemd/system/minikube-start.service
+[Unit]
+Description=Start Minikube
+After=docker.service
+Requires=docker.service
+
+[Service]
+Type=oneshot
+User=ec2-user
+ExecStart=/usr/local/bin/minikube start --driver=none
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable the service to run on boot
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable minikube-start.service
