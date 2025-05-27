17ab079d16ff34bc7a731f119cd453fb061cf72c
mandavikas17 <vikasmanda1249@gmail.com>
2025-05-27 19:24:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,26 @@ resource "aws_instance" "minikube_ec2" {
 
   user_data              = file("userdata.sh")
 
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.2"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.05.85801"
+  replicas = 1
+  upgrader_enabled = true
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
   tags = {
     Name = "MinikubeEC2_delegate"
   }
