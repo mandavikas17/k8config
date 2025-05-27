e5c98d6a0af1e1d0aff6f2e721552f16da836ac4
mandavikas17 <vikasmanda1249@gmail.com>
2025-05-27 15:54:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_security_group" "minikube_sg" {
+  name        = "minikube-sg"
+  description = "Allow NodePort and web access"
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow NodePort
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Optional for HTTP apps
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Optional for HTTPS apps
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "minikube_ec2" {
+  ami                    = "ami-0e58b56aa4d64231b" 
+  instance_type          = "t2.medium"
+  key_name               = "ansible"
+  security_groups        = [aws_security_group.minikube_sg.name]
+  associate_public_ip_address = true
+
+  user_data              = file("userdata.sh")
+
+  tags = {
+    Name = "MinikubeEC2_delegate"
+  }
+}
